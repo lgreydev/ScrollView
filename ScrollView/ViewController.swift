641a180158591a3e82bf94385b9023609153646e
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-01-24 20:31:12 +0200

    working scroll view

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = CGSize(width: 2880, height: 1620)
+        
+        guard let url = URL(string: "https://www.apple.com/newsroom/images/live-action/rosenthaler-strasse-store-opening/Apple_nso-rosenthaler-strasse_Hero_11302021_Full-Bleed-Image.jpg.large_2x.jpg") else { return }
+        
+        do {
+            let imageData = try Data(contentsOf: url)
+            guard let image = UIImage(data: imageData) else { fatalError() }
+            let imageView = UIImageView(image: image)
+            
+            scrollView.contentSize = imageView.frame.size
+            scrollView.minimumZoomScale = 0.1
+            scrollView.addSubview(imageView)
+        } catch let errorData {
+            print(errorData.localizedDescription)
+        }
+        
+    
     }
 
-
 }
-
