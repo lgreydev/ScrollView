789716dde679359a2f8471f12abad42a8ed78099
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2022-01-25 10:51:01 +0200

    Zoom scale

--- ScrollView/ImageScrollView.swift
+++ ScrollView/ImageScrollView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ImageScrollView: UIScrollView {
+class ImageScrollView: UIScrollView, UIScrollViewDelegate {
 
     private var imageZoomView: UIImageView!
     
@@ -19,7 +19,39 @@ class ImageScrollView: UIScrollView {
         configureFor(image.size)
     }
     
-    func configureFor(_ imageSize: CGSize) {
+    private func configureFor(_ imageSize: CGSize) {
         self.contentSize = imageSize
+//        self.minimumZoomScale = 0.1
+//        self.maximumZoomScale = 5.0
+        setCurrentMaxMinZoomScale()
+    }
+    
+    private func setCurrentMaxMinZoomScale() {
+        let boundsSize = self.bounds.size
+        let imageSize = imageZoomView.bounds.size
+        
+        let xScale = boundsSize.width / imageSize.width
+        let yScale = boundsSize.height / imageSize.height
+        
+        let minScale = min(xScale, yScale)
+        
+        self.minimumZoomScale = minScale
+    }
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.delegate = self
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        self.imageZoomView
     }
 }
